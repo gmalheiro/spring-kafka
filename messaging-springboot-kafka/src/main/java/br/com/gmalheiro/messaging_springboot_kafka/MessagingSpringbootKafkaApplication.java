3c2c218c7c75a918f5727c50720500de220156e0
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-21 19:26:52 -0300

    feat: add spring project

---
+++ messaging-springboot-kafka/src/main/java/br/com/gmalheiro/messaging_springboot_kafka/MessagingSpringbootKafkaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gmalheiro.messaging_springboot_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingSpringbootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingSpringbootKafkaApplication.class, args);
+	}
+
+}
